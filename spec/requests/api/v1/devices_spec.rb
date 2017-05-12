34d884c80668778e056fa36d9ea191af7899281f
Arman Jon Villalobos <me@armanjon.com>
2017-05-12 17:49:37 +0800

    Add device controller with index action

---
+++ spec/requests/api/v1/devices_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Devices API', type: :request do
+  describe 'GET /api/v1/devices' do
+
+    context 'pulls a list of devices' do
+      before(:each) do
+        @devices = FactoryGirl.create_list(:device, 5)
+        get '/api/v1/devices'
+        @json = JSON.parse(response.body)
+      end
+
+      it 'returns a 200 status code' do
+        expect(response).to be_success
+      end
+
+      it 'returns all devices' do
+        expect(@json.length).to eq(5)
+      end
+    end
+
+  end
+end
